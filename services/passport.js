1f6d6bd1250731c4e3181cbd7765a2a8254e2cd3
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-03-04 07:40:17 -0600

    save model instances

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -8,7 +11,5 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
 },
     (accessToken, refreshToken, profile, done) => {
-        console.log('aceess token', accessToken)
-        console.log('refresh token', refreshToken)
-        console.log('profile', profile)
+        new User({ googleId: profile.id }).save()
     }))
