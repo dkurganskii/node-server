86b9731ba30509136b08d65aff0e1f4fc87acf58
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-03-13 07:06:17 -0600

    enable cookies

--- services/passport.js
+++ services/passport.js
@@ -5,8 +5,15 @@ const keys = require('../config/keys')
 
 const User = mongoose.model('users')
 
-passport.serializeUser((user, done)=>{
-done(null, user.id)
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
 })
 
 passport.use(new GoogleStrategy({
@@ -23,7 +30,7 @@ passport.use(new GoogleStrategy({
                 } else {
                     // we don't have a user record with this ID, make a new record
                     new User({ googleId: profile.id }).save()
-                    .then(user => done(null, user))
+                        .then(user => done(null, user))
                 }
             })
     }))
